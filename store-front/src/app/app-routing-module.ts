a9b66bff1a33dc225336fb43947180f2f6b58884
Guilherme Araujo Silva <guilherme.silva@proway.treina>
2025-10-27 22:20:17 -0300

    feat(produto): Ajuste de layout

--- store-front/src/app/app-routing-module.ts
+++ store-front/src/app/app-routing-module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductList } from './components/product/product-list/product-list';
+import { ProductForm } from './components/product/product-form/product-form';
 
 const routes: Routes = [
   {path: 'produtos', component: ProductList},
+  {path: 'produto/novo', component: ProductForm},
+  {path: 'produto/editar/:id', component: ProductForm},
   {path: '', redirectTo: '/produtos', pathMatch: 'full'}
 ];
 
--- store-front/src/app/components/product/product-form/product-form.ts
+++ store-front/src/app/components/product/product-form/product-form.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IProduto } from '../../../interfaces/IProduto';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../../services/ProductService';
 
 @Component({
   selector: 'app-product-form',
@@ -7,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrl: './product-form.css',
 })
 export class ProductForm {
+
+  produto: IProduto = {
+    id: 0,
+    nome: '',
+    preco: 0,
+    quantidade: 0
+  };
+
+  isEdicao: boolean = false;
+
+  constructor(
+    private produtoService: ProductService,
+    private activeRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.activeRoute.snapshot.paramMap.get('id'));
+    if (id && id != 0) {
+      this.isEdicao = true;
+      this.produtoService.findById(id).subscribe(response => {
+        this.produto = response;
+      });
+    }
+  }
+
+  save(): void {
+    if (this.isEdicao) {
+      this.produtoService.update(this.produto).subscribe(() => {
+        this.router.navigate(['/produtos']);
+      });
+    } else {
+      this.produtoService.add(this.produto).subscribe(() => {
+        this.router.navigate(['/produtos']);
+      });
+    }
+  }
 
 }
